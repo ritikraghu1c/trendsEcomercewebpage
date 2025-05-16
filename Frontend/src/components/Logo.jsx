760e11482a15486cbf446113b9039c669dcb09d5
ritikraghu1c <158844354+ritikraghu1c@users.noreply.github.com>
2025-05-16 23:04:28 +0530

    Initial commit with Frontend and Backend

---
+++ Frontend/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assest/trends-high-resolution-logo (1).png'
+
+const Logo = ({w,h}) => {
+  return (
+    <img src={logo} alt="logo" className='w-[120px] h-[50px]'/>
+
+  )
+}
+
+export default Logo

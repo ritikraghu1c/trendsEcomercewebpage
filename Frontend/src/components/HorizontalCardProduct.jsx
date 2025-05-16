17e9f1d7d0d471f4fdb85f95b2f529f30154ab30
ritikraghu1c <158844354+ritikraghu1c@users.noreply.github.com>
2025-05-17 01:50:38 +0530

    Update HorizontalCardProduct.jsx

--- Frontend/src/components/HorizontalCardProduct.jsx
+++ Frontend/src/components/HorizontalCardProduct.jsx
@@ -78,7 +78,16 @@ const HorizontalCardProduct = ({category, heading}) => {
                 return(
                     <Link to={"product/"+product?._id} className='w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex'>
                         <div className='bg-slate-200 h-full p-4 min-w-[120px] md:min-w-[145px]'>
-                            <img src={product.productImage[0]?.replace("http://", "https://")} className='object-scale-down h-full hover:scale-110 transition-all'/>
+                          <img
+                                      src={
+                                      product.productImage[0]?.includes("http://")
+                                          ? product.productImage[0].replace("http://", "https://")
+                                    : product.productImage[0]
+                                      }
+                                onLoad={() => console.log("Loaded Image:", product.productImage[0])}
+                                className='object-scale-down h-full hover:scale-110 transition-all'
+                                  />
+
                         </div>
                         <div className='p-4 grid'>
                             <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black'>{product?.productName}</h2>

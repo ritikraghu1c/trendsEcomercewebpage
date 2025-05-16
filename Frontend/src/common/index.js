2063e47a11a30a90b3e1a0911ca889b974401913
ritikraghu1c <158844354+ritikraghu1c@users.noreply.github.com>
2025-05-17 03:33:27 +0530

    new code

--- Frontend/src/common/index.js
+++ Frontend/src/common/index.js
@@ -1,6 +1,9 @@
 
+const backendDomin = process.env.REACT_APP_BACKEND_URL;
 
-const backendDomin ="https://treds-ecomerce-mern-backend.vercel.app"
+
+
+
 const SummaryApi = {
     signUP : {
         url : `${backendDomin}/api/signup`,
